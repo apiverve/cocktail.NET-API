e2b049f9073c6be94fe6ffef2627d293c72def67
APIVerve Team <hello@apiverve.com>
2024-05-14 15:22:15 -0500

    Bug fixes and Improvements

---
+++ cocktailQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class cocktailQueryOptions {
+public string name { get; set; }
+public string ingredient { get; set; }
+}
+
+}
