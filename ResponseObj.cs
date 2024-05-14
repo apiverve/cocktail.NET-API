3e0e1e7defe978e898f51b4a292721f9080c1b04
APIVerve Team <hello@apiverve.com>
2024-05-14 15:22:18 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,67 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class ingredients
+{
+    [JsonProperty("unit")]
+    public string unit { get; set; }
+
+    [JsonProperty("amount")]
+    public int amount { get; set; }
+
+    [JsonProperty("ingredient")]
+    public string ingredient { get; set; }
+
+}
+
+public class cocktails
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("glass")]
+    public string glass { get; set; }
+
+    [JsonProperty("category")]
+    public string category { get; set; }
+
+    [JsonProperty("ingredients")]
+    public ingredients[] ingredients { get; set; }
+
+    [JsonProperty("garnish")]
+    public string garnish { get; set; }
+
+    [JsonProperty("preparation")]
+    public string preparation { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("count")]
+    public int count { get; set; }
+
+    [JsonProperty("filteredOn")]
+    public string filteredOn { get; set; }
+
+    [JsonProperty("cocktails")]
+    public cocktails[] cocktails { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
